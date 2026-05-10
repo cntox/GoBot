d8e5f1eb9c8a57204199018f839779b7a8215394
cntox <d72379323@gmail.com>
2026-05-10 12:17:58 +0530

    Update main.go

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
     client.On(telegram.OnMessage, func(message *telegram.NewMessage) error {
         message.Reply("Hello from Gogram!")
         return nil
-    }, telegram.IsGroup) // waits for private messages only
+    }, telegram.IsCommand("start")) // waits for private messages only
 
     client.Idle() // block main goroutine until client is closed
 }
