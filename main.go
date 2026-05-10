b36be0ec742fffa202968da807d3d3fd9858e3a6
cntox <d72379323@gmail.com>
2026-05-10 11:47:59 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "github.com/amarnathcjd/gogram/telegram"
+
+func main() {
+    client, err := telegram.NewClient(telegram.ClientConfig{
+        AppID: 26593951, AppHash: "98d8a63d74b4082b129defe4f941ab4d",
+    })
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    client.Conn() // important, establishes connection to Telegram servers
+
+    // Login as bot or user
+    client.LoginBot("8793661673:AAEn5NK7sJ-dV328XBEVgeWhoDWbAICLzUI") 
+    // or client.Login("<phone-number>") for user account
+    // or client.AuthPrompt() for interactive login
+
+    // Handle incoming messages
+    client.On(telegram.OnMessage, func(message *telegram.NewMessage) error {
+        message.Reply("Hello from Gogram!")
+        return nil
+    }, telegram.FilterPrivate) // waits for private messages only
+
+    client.Idle() // block main goroutine until client is closed
+}
