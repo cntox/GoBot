e6fa8962d6b32f0d8bee0b24e9e3361fa85833b0
ʙᴀᴅ ᴍᴜɴᴅᴀ ❤️ <kiran6jeet8@gmail.com>
2026-05-18 14:40:46 +0530

    Update handlers.go

--- modules/handlers.go
+++ modules/handlers.go
@@ -391,7 +391,7 @@ func handleQuizBotMessage(client *telegram.Client, m *telegram.NewMessage) error
 			Quiz:     true,
 			Closed:   true,
 		},
-		CorrectAnswers: []int32{int32(correctIdx)},
+		CorrectAnswers: [][]byte{{byte(correctIdx)}},
 	}
 	if _, err := client.EditMessage(localTarget, int32(sentMsg.ID), closedPoll); err != nil {
 		log.Printf("Failed to close poll: %v", err)
